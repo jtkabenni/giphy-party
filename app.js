c946ce54ebe39271da93296d3f544fc50e552540
Jenny Takahara <jenny.takahara@gmail.com>
2023-03-08 22:24:22 -0500

    updates to css, js, html

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-console.log("Let's get this party started!");
 const gifSection = document.querySelector("#gifs");
 const searchForm = document.querySelector("#search-field");
 const searchButton = document.querySelector("#submit");
 const removeButton = document.querySelector("#remove-images");
 
+//handles search event
 function searchHandler(evt) {
   evt.preventDefault();
-  console.log(searchForm.value);
   if (searchForm.value !== "") {
     getGifPls(searchForm.value);
   }
   searchForm.value = "";
 }
-
+//sends request to giphy API
 async function getGifPls(term) {
   const result = await axios.get(
     `https://api.giphy.com/v1/gifs/random?api_key=wBi71TLOWLfKHH88c8jwA7h3TJvQFZ4K&tag=${term}&limit=1`
   );
-  const imgUrl = result.data.data.images.downsized_medium.url;
-  console.log(result.data.data.images.downsized_medium.url);
-  appendGif(imgUrl);
+  const url = result.data.data.images.downsized_medium.url;
+  appendGif(url);
 }
-
+//appends random gif to gif section
 function appendGif(url) {
-  console.log(url);
   const div = document.createElement("div");
   const gif = document.createElement("img");
   div.classList.add("gif");
@@ -31,10 +28,10 @@ function appendGif(url) {
   div.append(gif);
   gifSection.append(div);
 }
-
+//deletes gifs
 function removeGifs() {
-  console.log("Delete");
   gifSection.innerHTML = "";
+  searchForm.value = "";
 }
 
 searchForm.addEventListener("submit", searchHandler);
